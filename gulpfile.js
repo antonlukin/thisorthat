fb16d9012c199ffaecaa0d6266954f06b5b6c37a
Anton Lukin <anton@lukin.me>
2020-07-12 16:44:32 +0400

    add simple landing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,52 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const cleanCss = require('gulp-clean-css');
+const sassGlob = require('gulp-sass-glob');
+const plumber = require('gulp-plumber');
+const flatten = require('gulp-flatten');
+const prefix = require('gulp-autoprefixer');
 const connect = require('gulp-connect');
 const template = require('gulp-md-template');
+
+
+const path = {
+  source: 'src/',
+  assets: 'public/assets/'
+}
+
+gulp.task('styles', (done) => {
+  gulp.src([path.source + '/styles/app.scss'])
+    .pipe(plumber())
+    .pipe(sassGlob({
+      allowEmpty: true
+    }))
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(prefix())
+    .pipe(concat('styles.min.css'))
+    .pipe(cleanCss())
+    .pipe(gulp.dest(path.assets))
+
+  done();
+});
+
+
+gulp.task('watch', (done) => {
+  gulp.watch('./src/**/*', gulp.series('styles'));
+
+  done();
+});
+
+
+gulp.task('images', (done) => {
+  gulp.src([path.source + '/images/*.{jpg,png}'])
+    .pipe(flatten())
+    .pipe(gulp.dest(path.assets + '/images/'));
+
+  done();
+});
 
 
 gulp.task('markdown', (done) => {
@@ -20,5 +66,5 @@ gulp.task('connect', (done) => {
   done();
 })
 
-gulp.task('default', gulp.parallel('connect'));
+gulp.task('default', gulp.parallel('styles', 'images', 'watch', 'connect'));
 
